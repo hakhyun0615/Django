4819d7f386e6c652d41e866451eb4f992a3650a5
kimhakhyun <hakhyun0615@naver.com>
2022-07-22 13:13:37 +0900

    main

---
+++ costaurant/foods/views.py
@@ -0,0 +1,13 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+def index(request):
+    today = datetime.today().date()
+    context = {"date":today}
+    return render(request,'foods/index.html',context=context)
+
+def chicken(request):
+    return render(request,'foods/chicken.html')
